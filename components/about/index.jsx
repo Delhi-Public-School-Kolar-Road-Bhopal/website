c7a2d9dbc3f129c1b565168f7e5a80d1b4a1dbfe
TheHamsterDog <shreshth@shreshthverma.me>
2022-11-04 21:35:16 +0530

    feat: responsiveness

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -50,7 +50,7 @@ const organizers = [{
     volunteer: false,
     image: "https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     designation: "Team leader of Scio",
-    profile: "Inquizitive and pragmatic, i have participated in many inter school quizzes and cryptic hunts"
+    profile: "Inquizitive and pragmatic, he has participated in many inter school quizzes and cryptic hunts"
 },
 {
     name: 'Aarush Shrivastava',
