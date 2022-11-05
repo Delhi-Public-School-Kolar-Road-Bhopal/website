73d82eed8616f3c8d19a8d771c88fd5e076afe1c
TheHamsterDog <shreshth@shreshthverma.me>
2022-11-05 19:53:53 +0530

    feat: added short descriptions

--- components/home/infoSection.jsx
+++ components/home/infoSection.jsx
@@ -11,6 +11,7 @@ const InfoSection = () => {
         The questions in the first round will carry 10 scores each, the ones in the second round and the data structures round will carry 20 scores each, and the ones in the third round will carry 30 scores each. There will be an opportunity to gain bonus scores.
         Awards and separate certificates of recognition will be given to participants who’d be ranked #1, #2, and #3, as well as participants with outstanding programming knowledge who, for some reason, couldn’t be allocated any of the aforementioned ranks
         `,
+        short: "The dawn of this century came with the advent of a new sport, competitive programming. This competition challenges participants' ability to produce optimized, efficient, scalable, and readable solutions to tricky questions in four highly challenging rounds. To excel at this competition, a person needs to have outstanding skills and knowledge about data structures and algorithms and either python or the javascript programming language. However, to enjoy this event, there are no such requirements. We have curated a diverse array of questions that challenge and force participants to think outside the box, some of which require little expertise while others require exceptionalism. ",
         venue: "Senior Computer Lab",
         timing: 'The event will last for approximately 3 hours, without accounting for breaks. The first round will last for 30 minutes. The second round will last for an hour. The third round will last for an hour and thirty minutes. You will be allowed to work on questions from the previous round(s) and the Data Structure during the second and third rounds',
         tps: 1,
@@ -32,6 +33,7 @@ const InfoSection = () => {
         Any type of Manipulation is not allowed however basic post-processing will be considered.
         Participants have to upload their original work
         `,
+        short: "Photo perfecta is the perfect opportunity for all the creative photographers out there to showcase their masterpiece and compete with other photographers as well.",
         venue: "Online",
         timing: 'The participants will be given one week to submit their photographs. The last date for submission is 3rd November 2022',
         tps: 2,
@@ -53,6 +55,7 @@ const InfoSection = () => {
         Coaches are not allowed
         `,
         venue: "Online",
+        short: "E-sports is now as mainstream as physical sports. Being one the most popular competitive games on this planet, Valorant is the game that this event will use to test your skills and your team's coordination.",
         timing: "As per the Brackets",
         tps: 1,
         participants: "6(5 players + 1 substitute)"
@@ -70,6 +73,7 @@ const InfoSection = () => {
         1hr 30min will be given for editing the video.
         A tour of the school will be provided`,
         venue: "School Premises and Middle Computer Lab",
+        short: "Visual forms of art dominates the world today, film making is everywhere, the world is filled with art -  the art of capturing the world and telling a story through a simple lens but conveying so much more, the art of expressing complex emotions and feelings from the simplest of objects .This event asses the ability to capture the externalities of the world  through visual media. Filmskaping will need creativity and imagination, a keen eye for details and a critical mind. This competition will also test the the participant’s knowledge and competence in technical skills like video and sound editing, and cinematography.",
         timing: 'The event will last for approximately 2 hours and 30 minutes, without accounting for breaks',
         tps: 1,
         participants: 2
@@ -179,12 +183,14 @@ const InfoSection = () => {
                         </div>
                     </div>
                     <label className="info-section-each" htmlFor={index}>
-                        <h3>
-                            {a.name}
-                        </h3>
-                        <p className="info-section-description">
-                            {a.short}
-                        </p>
+                        <div>
+                            <h3>
+                                {a.name}
+                            </h3>
+                            <p className="info-section-description">
+                                {a.short}
+                            </p>
+                        </div>
                         <h4 className="info-section-each-label" >
 
                             Click to Learn More
