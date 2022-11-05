7c0513a5b03cafa0c4e95b749c62d59d1ba6f32b
Angad7600123 <singh4anga@gmail.com>
2022-11-05 15:28:59 +0530

    updated rules perfecta

--- components/home/infoSection.jsx
+++ components/home/infoSection.jsx
@@ -9,7 +9,7 @@ const InfoSection = () => {
         The students are allowed to use JS/python; No additional libraries, modules, or frameworks will be allowed.
         There will be 4 levels—Round #1, Round #2, Round #3, and the data structure round. the data structure round will run parallel to the other rounds.
         The questions in the first round will carry 10 scores each, the ones in the second round and the data structures round will carry 20 scores each, and the ones in the third round will carry 30 scores each. There will be an opportunity to gain bonus scores.
-        Awards and separate certificates of recognition will be given to participants who’d be ranked #1, #2, and #3, as well as participants with outstanding programming knowledge who, for some reason, couldn’t be allocated any of the aforementioned ranks.
+        Awards and separate certificates of recognition will be given to participants who’d be ranked #1, #2, and #3, as well as participants with outstanding programming knowledge who, for some reason, couldn’t be allocated any of the aforementioned ranks
         `,
         venue: "Senior Computer Lab",
         timing: 'The event will last for approximately 3 hours, without accounting for breaks. The first round will last for 30 minutes. The second round will last for an hour. The third round will last for an hour and thirty minutes. You will be allowed to work on questions from the previous round(s) and the Data Structure during the second and third rounds',
@@ -23,7 +23,7 @@ const InfoSection = () => {
         description: `
         This is an online event.
         This is an individual event.
-        THe participants have to submit their photographs online on the provided google form.
+        The participants have to submit their photographs online on the provided google form.
         The theme will be provided on 13th november. The photograph has to be clicked between 13-15 November.
         The last date and time for submission is 15th november 2022, 23:59:59.
         Each participant has to send 2 photographs; 1 photograph for each theme.
@@ -50,7 +50,7 @@ const InfoSection = () => {
         It is advised that players take screenshots of game scores.
         only 3 timeouts are allowed.1 before the half, 1 after the half and in overtime.
         If a technical problem arises 10min timeout will be given.
-        coaches are not allowed
+        Coaches are not allowed
         `,
         venue: "Online",
         timing: "Flexible",
