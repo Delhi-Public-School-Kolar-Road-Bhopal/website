010216c4adeab12e9e0f4b5774e3df4ad45c23c9
Angad7600123 <singh4anga@gmail.com>
2022-11-05 23:29:42 +0530

    reverted

--- components/about/index.jsx
+++ components/about/index.jsx
@@ -53,7 +53,7 @@ const organizers = [{
     name: "Rishabh Kothari",
     core: false,
     volunteer: false,
-    image: "https://toppng.com/uploads/preview/and-blank-effect-transparent-11546868080xgtiz6hxid.png",
+    image: "https://images.unsplash.com/photo-1582266255765-fa5cf1a1d501?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
     designation: "Team leader of Scio",
     profile: "Inquizitive and pragmatic, he has participated in many inter school quizzes and cryptic hunts"
 },
