8792ea00f839f7c9b2aeb89190d9b3b1f9a864fc
Angad7600123 <singh4anga@gmail.com>
2022-11-14 10:18:40 +0530

    feat: added brochure link:

--- components/home/venue_timing.jsx
+++ components/home/venue_timing.jsx
@@ -1,6 +1,7 @@
 
+import Brochure from './brochure'
 const Venue = () => {
-    let m = { "Offline events will be held on": "Wednesday, 16th November, 2022", "Online events will start on": "Sunday, 13th November, 2022", "Results will be declared on": "Wednesday, 16th of November, 2022", "The registrations for online events have been": "Closed", "The registration deadline for OFFLINE events is": "Tuesday, 15th of November, 2022", "The total monetary value of prizes that will be awarded across all 6 events is":"Over ₹10,000"};
+    let m = { "Offline events will be held on": "Wednesday, 16th November, 2022", "Online events will start on": "Sunday, 13th November, 2022", "Results will be declared on": "Wednesday, 16th of November, 2022", "The registrations for online events have been": "Closed", "The registration deadline for OFFLINE events is": "Tuesday, 15th of November, 2022", "The total monetary value of prizes that will be awarded across all 6 events is": "Over ₹10,000" };
     let keys = Object.keys(m);
     return (
         <div className="container">
@@ -13,6 +14,8 @@ const Venue = () => {
             </div>
             <div className="container-venue">
                 <div className="mapouter"><div className="gmap_canvas"><iframe className="gmap_iframe" width="100%" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=DPS Kolar Road, Bhopal&amp;t=h&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://www.kokagames.com/fnf-friday-night-funkin-mods/">FNF</a></div><style></style></div>
+
+                <Brochure />
             </div>
         </div>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -13,6 +13,7 @@ const Home = () => {
     <Venue />
     <InfoSection /> 
     <Register />
+    
   </div>
   )
 }
